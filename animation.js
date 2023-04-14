11a668a7aae93ccc45c95c65b00bbf71ed919392
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-04-14 10:47:24 +0200

    update css html added js animation

---
+++ animation.js
@@ -0,0 +1,31 @@
+const projects = document.querySelectorAll('.project');
+let isInViewport = false;
+
+function handleScroll() {
+  projects.forEach((project) => {
+    // Vérifie si le projet est dans la vue de l'utilisateur
+    const projectTop = project.offsetTop;
+    const projectHeight = project.offsetHeight;
+    const viewportHeight = window.innerHeight;
+    const scrollTop = window.pageYOffset;
+    const scrollBottom = scrollTop + viewportHeight;
+
+    if (scrollBottom > projectTop && scrollTop < projectTop + projectHeight) {
+      if (!isInViewport) {
+        // Si le projet est dans la vue et n'était pas dans la vue précédemment, active l'animation
+        isInViewport = true;
+        project.classList.add('in-view');
+      }
+    } else {
+      if (isInViewport) {
+        // Si le projet n'est plus dans la vue et était dans la vue précédemment, désactive l'animation
+        isInViewport = false;
+        project.classList.remove('in-view');
+      }
+    }
+  });
+}
+
+window.addEventListener('scroll', handleScroll);
+
+
---
+++ loader.js
@@ -0,0 +1,7 @@
+// Ajouter une classe "hide" à l'élément de chargement
+document.querySelector('p[aria-label="Loading"]').classList.add('hide');
+
+// Enlever la classe "hide" de l'élément de contenu
+document.querySelector('.hexagon').classList.remove('hide');
+
+
